34e084cb133b4a3656bebde7a72304d8a8d0d93d
Maximilian Wasylow <maxw@maxpc.magog.ecric.org.uk>
2017-02-21 15:14:49 +0000

    debugging

--- pizza.py
+++ pizza.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+t = 'T'
+m = 'M'
+
 class Pizza(object):
 
   def __init__(self, pizza):
@@ -11,17 +14,30 @@ class Pizza(object):
   def populatePizza(self):
     cid = 1
     for (x,y), cell in np.ndenumerate(self.pslicepos):
-        cell = PSlice(x,y,1,1)
+        cell = PSlice(x,y,1,1, self)
 
   def findNeighbours(self, pslice):
     ngbs = []
+    print pslice
     if pslice.y != 0:
-      ngbs.append([i for i in np.nditer(self.pslicepos[pslice.y-1:pslice.y, pslice.x:pslice.xe])])
+      print 1
+      print pslice.y-1, pslice.y, pslice.x, pslice.xe
+      print self.pslicepos[pslice.y-1:pslice.y, pslice.x:pslice.xe]
+      ngbs.append([i for i in np.nditer(self.pslicepos[pslice.x:pslice.xe, pslice.y-1:pslice.y])])
     if pslice.x != 0:
-      ngbs.append([i for i in np.nditer(self.pslicepos[pslice.y:pslice.ye, pslice.x-1:pslice.x])])
-    if pslice.ye < self.pslicepos.shape[1]:
-      ngbs.append([i for i in np.nditer(self.pslicepos[pslice.ye+1:pslice.ye+2, pslice.x:pslice.xe])])
-    if pslice.xe < self.pslicepos.shape[0]:
+      print 2
+      print pslice.y, pslice.ye, pslice.x-1, pslice.x
+      print self.pslicepos[pslice.y:pslice.ye, pslice.x-1:pslice.x]
+      ngbs.append([i for i in np.nditer(self.pslicepos[pslice.x-1:pslice.x, pslice.y:pslice.ye])])
+    if pslice.ye+1 < self.pslicepos.shape[1]:
+      print 3
+      print pslice.ye+1, pslice.ye+2, pslice.x, pslice.xe
+      print self.pslicepos[pslice.ye+1:pslice.ye+2, pslice.x:pslice.xe]
+      ngbs.append([i for i in np.nditer(self.pslicepos[pslice.x:pslice.xe, pslice.ye+1:pslice.ye+2])])
+    if pslice.xe+1 < self.pslicepos.shape[0]:
+      print 4
+      print pslice.y, pslice.ye, pslice.xe+1, pslice.xe+2
+      print self.pslicepos[pslice.xe+1:pslice.xe+2, pslice.y:pslice.ye]
       ngbs.append([i for i in np.nditer(self.pslicepos[pslice.y:pslice.ye, pslice.xe+1:pslice.xe+2])])
     return ngbs
 
@@ -29,18 +45,22 @@ class PSlice(object):
 
   nextid = 1
 
-  def __init__(self, x, y, xlen, ylen):
+  def __init__(self, x, y, xlen, ylen, pizza):
     self.id = self.nextID()
     self.x = x
     self.y = y
     self.xe = x + xlen
     self.ye = y + ylen
-    self.neighbours = Pizza.findNeighbours(self)
+    self.pizza = pizza
+    self.neighbours = pizza.findNeighbours(self)
+
+  def __str__(self):
+    return "<PSlice id={} x={} y={} xe={} ye={}>".format(self.id, self.x, self.y, self.xe, self.ye)
 
   def score(self):
     pass
 
   def nextID(self):
-    tmpid = nextid
-    self.nextid += 1
+    tmpid = PSlice.nextid
+    PSlice.nextid += 1
     return tmpid
